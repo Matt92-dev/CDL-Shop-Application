8b034ef35091a5856f1c8fae001525992f0784fb
Matt92-dev <69991489+Matt92-dev@users.noreply.github.com>
2022-03-09 17:42:55 +0000

    Logic created for the special offers

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
+//Item A Globals
 var itemA = 0.5;
+var itemAOfferCount = 0;
+var itemAOfferInt = 3;
+var itemAOfferAmount = .3;
+
+//Item B Globals
 var itemB = 0.3;
+var itemBOfferCount = 0;
+var itemBOfferInt = 2;
+var itemBOfferAmount = .15;
+
+//Item C Globals
 var itemC = 0.2;
+
+//Item D Globals
 var itemD = 0.12;
-var currentTotal = 0;
+
+
+var globalItemCount = 0;
 var currentTotal = 0;
 
 jQuery(document).ready(function () {
 
     //Item A click function
     $('#item-a').click(function () {
-        currentTotal += itemA;
+        itemAOfferCount += 1;
+        if (itemAOfferCount == itemAOfferInt) {
+            globalItemCount += 1;
+            itemAOfferCount = 0;
+            currentTotal += itemAOfferAmount;
+            alert('free item');
+        } else {
+            globalItemCount += 1;
+            currentTotal += itemA;
+        }
+        $('#item-count').text(globalItemCount);
         $('#total').text(currentTotal.toFixed(2));
     });
-    
+
     //Item B click function
     $('#item-b').click(function () {
-        currentTotal += itemB;
+        itemBOfferCount += 1;
+        if (itemBOfferCount == itemBOfferInt) {
+            globalItemCount += 1;
+            itemBOfferCount = 0;
+            currentTotal += itemBOfferAmount;
+            alert('free item');
+        } else {
+            globalItemCount += 1;
+            currentTotal += itemB;
+        }
+        $('#item-count').text(globalItemCount);
         $('#total').text(currentTotal.toFixed(2));
     });
-    
+
     //Item C click function
     $('#item-c').click(function () {
+        globalItemCount += 1;
         currentTotal += itemC;
+        $('#item-count').text(globalItemCount);
         $('#total').text(currentTotal.toFixed(2));
     });
-    
+
     //Item D click function
     $('#item-d').click(function () {
+        globalItemCount += 1;
         currentTotal += itemD;
+        $('#item-count').text(globalItemCount);
         $('#total').text(currentTotal.toFixed(2));
     });
 });
